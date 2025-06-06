901ceae36c9fa5f2f6ac9ab3aaaee727fb75ba29
Umberto Amenta <umberto.amenta@hotmail.it>
2025-06-06 19:48:25 +0200

    feat: snack 2 - useDate()

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import useSwitch from "./snacks/snack1";
+import useDate from "./snacks/Snack2";
 
 export default function App() {
   const [isOn, toggle] = useSwitch();
 
+  const currentDate = useDate();
+
   return (
-    <div>
-      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
-      <button onClick={toggle}>Cambia Stato</button>
-    </div>
+    <>
+      <div>
+        <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+        <button onClick={toggle}>Cambia Stato</button>
+      </div>
+
+      <div>
+        <h1>Data e ora attuali:</h1>
+        <p>
+          {currentDate.date} - {currentDate.time}
+        </p>
+      </div>
+    </>
   );
 }
---
+++ src/snacks/Snack2.jsx
@@ -0,0 +1,20 @@
+// 🏆 Snack 2: useDate() – Ottenere la Data Attuale
+// Creare un custom hook che restituisca la data e l'ora correnti, aggiornandosi automaticamente ogni secondo.
+
+// Cosa deve fare?
+
+// Restituisce un oggetto con data e ora formattata.
+// Si aggiorna automaticamente ogni secondo.
+// Usa useEffect() per gestire l’aggiornamento.
+
+import { useEffect, useState } from "react";
+
+export default function useDate() {
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => setNow(new Date()), 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+  return { date: now.toLocaleDateString(), time: now.toLocaleTimeString() };
+}
