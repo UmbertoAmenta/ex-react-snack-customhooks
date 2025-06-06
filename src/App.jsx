45543ac8aec8d2d78756194d92b73afa862d1a6d
Umberto Amenta <umberto.amenta@hotmail.it>
2025-06-06 19:30:59 +0200

    init project + snack1 - useSwitch()

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import useSwitch from "./snacks/snack1";
+
+export default function App() {
+  const [isOn, toggle] = useSwitch();
+
+  return (
+    <div>
+      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+      <button onClick={toggle}>Cambia Stato</button>
+    </div>
+  );
+}
